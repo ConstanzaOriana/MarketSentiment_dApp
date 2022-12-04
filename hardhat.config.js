73963d0d683981f04ea84bc8ee870926d1129af2
Constanza Oriana <cotyoriana95@gmail.com>
2022-12-04 09:50:58 -0300

    add_env_file

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,9 +5,6 @@ const dotenv = require('dotenv')
 
 dotenv.config();
 
-
-/** @type import('hardhat/config').HardhatUserConfig */
-
 module.exports = {
   solidity: "0.8.7",
   defaultNetwork: "mumbai",
